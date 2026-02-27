e8c2c5d74843881f0d69deb9d0c945b2d29cbaa5
Jana Schneiderbauer <SchneiderbauerJ21056@sus.htl-grieskirchen.at>
2026-02-27 08:37:57 +0100

    implemented the backend endpoints for food and the database table

--- foodie_backend/src/app.ts
+++ foodie_backend/src/app.ts
@@ -4,8 +4,8 @@ import 'dotenv/config';
 import fs from 'fs';
 import path from 'path';
 import { title } from 'process';
-import { Book } from './models/food';
 import sqlite3 from 'sqlite3';
+import { Food } from './models/food';
 
 const app = express();
 app.use(bodyParser.json());
@@ -23,60 +23,49 @@ const port = process.env.PORT || 3000;
 
 const db = new sqlite3.Database('foods.sqlite');
 
-const books: Book[] = [];
-
 function loadData(): void {
   if (fs.existsSync('foods.sqlite')) return;
   db.serialize(() => {
     //für jede Tabelle ein db.run()
     db.run('CREATE TABLE IF NOT EXISTS food (id INTEGER PRIMARY KEY, name TEXT, description TEXT, shopname TEXT, price REAL, unit TEXT)');
     db.run('CREATE TABLE IF NOT EXISTS orders (id INTEGER PRIMARY KEY, name TEXT)');
-    db.run('CREATE TABLE IF NOT EXISTS order_items (order_id INTEGER, food_id INTEGER, quantity INTEGER NOT NULL DEFAULT 1, PRIMARY KEY (order_id, food_id), FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE, FOREIGN KEY (food_id) REFERENCES foods(food_id))');
+    db.run('CREATE TABLE IF NOT EXISTS order_items (id INTEGER, food_id INTEGER, order_id INTEGER, quantity INTEGER NOT NULL DEFAULT 1, PRIMARY KEY (order_id, food_id), FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE, FOREIGN KEY (food_id) REFERENCES foods(id))');
+
+    fs.readFile(path.join(process.cwd(), 'dist/csv', 'foods.csv'), 'utf-8', (_, data) => {
+      const lines = data.split('\n');
+      lines.shift();
+      lines.forEach(line => {
+        //id,name,description,shopname,price,unit
+        if (line) {
+          const parts = line.split(',');
+          db.run('INSERT INTO food (name, description, shopname, price, unit) VALUES ($name, $description, $shopname, $price, $unit)', {
+            $name: parts[1],
+            $description: parts[2],
+            $shopname: parts[3],
+            $price: parts[4],
+            $unit: parts[5]
+          });
+        }
+      });
+    });
   });
 }
 
 loadData();
 
-app.get('/books', middleWareGet, (req: Request, res: Response) => {
-  db.all('SELECT id, title, author, year FROM book', (err, books: Book[]) => {
-    res.send(books);
+app.get('/foods', middleWareGet, (req: Request, res: Response) => {
+  db.all('SELECT id, name, description, shopname, price, unit FROM food', (err, foods: Food[]) => {
+    res.send(foods);
   });
 
 });
 
-app.get('/books{/:id}', middleWareGet, (req: Request, res: Response) => {
-  db.all('SELECT id, title, author, year FROM book', (err, books: Book[]) => {
-    res.send(books.find(x => x.id === (+req.params.id)));
+app.get('/foods{/:searchString}', middleWareGet, (req: Request, res: Response) => {
+  db.all('SELECT id, name, description, shopname, price, unit FROM food', (err, foods: Food[]) => {
+    res.send(foods.find(x => x.name.includes(req.params.searchString)));
   })
 });
 
-app.post('/books', (req: Request, res: Response) => {
-  const book: Book = req.body;
-  db.run('INSERT INTO book (title, author, year) VALUES ($title, $author, $year)', {
-    // $title: book.title,
-    // $author: book.author,
-    // $year: book.year
-  });
-  res.status(200).send();
-});
-
-app.put('/books{/:id}', (req: Request, res: Response) => {
-  const book: Book = req.body;
-  db.run('UPDATE book SET title = $title, author = $author, year = $year WHERE id = $id', {
-    // $id: req.params.id,
-    // $title: book.title,
-    // $author: book.author,
-    // $year: book.year
-  });
-  res.status(200).send();
-});
-
-app.delete('/books{/:id}', (req: Request, res: Response) => {
-  db.run('DELETE FROM book WHERE id = $id', {
-    $id: req.params.id,
-  });
-  res.status(200).send();
-});
 
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
